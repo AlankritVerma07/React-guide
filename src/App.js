dbcbdc5d58485409c74cf7fc0f3fad533a3fe3d4
Alankrit Verma <alankritverma.lko@gmail.com>
2020-11-28 15:25:18 -0800

    useState Introduction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>I'am React App!</h1>
-        <Person name="Max M">Hobbies:Playien</Person>
-        <Person name="Mike" />
-        <Person name="Sam" />
-      </div>
-    );
-  }
-}
+const app = (props) => {
+  const [personsState, setPersonsSState] = useState({
+    persons: [
+      { name: "Mike", age: 22 },
+      { name: "Michal", age: 24 },
+      { name: "Tom", age: 23 },
+    ],
+    otherState: "Some other value",
+  });
+  console.log(personsState);
+  const switchNameHandler = () => {
+    setPersonsSState({
+      persons: [
+        { name: "Sam", age: 29 },
+        { name: "James", age: 32 },
+        { name: "Joe", age: 42 },
+      ],
+      otherState: personsState.otherState, //since useSate() replaces the older state and does not merges it
+    });
+  };
+  return (
+    <div className="App">
+      <h1>I'am React App!</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>
+        Hobbies:Playien
+      </Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+};
 
-export default App;
+export default app;
