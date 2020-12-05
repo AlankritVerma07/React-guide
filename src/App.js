eb43ca29ce969197efa278eac044c4d62fde85a3
Alankrit Verma <alankritverma.lko@gmail.com>
2020-12-05 02:21:27 -0800

    Intro to Radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -52,6 +53,10 @@ class App extends Component {
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -73,6 +78,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
     }
     const classes = [];
     if (this.state.persons.length <= 2) classes.push("red"); //classes=["red"]
@@ -91,7 +100,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
 //-----------------------------------Assignment Sol.----------------------------------
 
