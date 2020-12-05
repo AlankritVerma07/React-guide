f4593bde237201bc3edea0bd7788b7464552a2c7
Alankrit Verma <alankritverma.lko@gmail.com>
2020-12-05 04:37:36 -0800

    Hover effect using styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 //import Radium, { StyleRoot } from "radium"; //we need StyleRoot for @mediaquries
+import styled from "styled-components";
 import Person from "./Person/Person";
+
+const StyledButton = styled.button`
+  background-color: ${(props) => (props.alt ? "red" : "green")};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => (props.alt ? "salmon" : "lightgreen")};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -77,11 +92,11 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = "red";
-      style[":hover"] = {
-        backgroundColor: "salmon",
-        color: "black",
-      };
+      // style.backgroundColor = "red";
+      // style[":hover"] = {
+      //   backgroundColor: "salmon",
+      //   color: "black",
+      // };
     }
     const classes = [];
     if (this.state.persons.length <= 2) classes.push("red"); //classes=["red"]
@@ -90,9 +105,12 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </button>
+        </StyledButton>
+        {/* <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button> -----> Using inline-style and Radium */}
         {persons}
       </div>
     );
