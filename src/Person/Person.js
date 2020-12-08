6ddb4564cb515164fe0f2b8157cd637520706d46
Alankrit Verma <alankritverma.lko@gmail.com>
2020-12-08 00:10:33 -0800

    Css Modules to dumb components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from "react";
-//import "./Person.css";
+import classes from "./Person.css";
 //import Radium from "radium";
-import styled from "styled-components";
+//import styled from "styled-components";
 
-const StyledDiv = styled.div`
+/* const StyledDiv = styled.div`
   width: 60%;
   margin: 16px auto;
   border: 1px solid rgb(49, 49, 49);
@@ -13,7 +13,7 @@ const StyledDiv = styled.div`
   @media (min-width: 500px) {
     width: 450px;
   }
-`;
+`; */
 
 const person = (props) => {
   // const style = {
@@ -23,13 +23,13 @@ const person = (props) => {
   // };
   return (
     //<div className="Person" style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I'm {props.age} years old
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
